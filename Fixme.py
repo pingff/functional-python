19d352d6c1a16c30eeeb18518e680d88ca6e437f
pingff <lfong24@students.claremontmckenna.edu>
2023-01-30 23:30:52 -0800

    updated fixme

--- Fixme.py
+++ Fixme.py
@@ -21,12 +21,12 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    return list(filter(lambda x: x % 2==0, range(0 , n+1)))
+    return list(filter(lambda x: x % 2 == 0, range(0, n+1)))
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of numbers from 0 to n inclusive that contain the digit 3.
 
     >>> threes(2)
     []
@@ -39,12 +39,12 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    return list(filter(lambda x: '3' in str(x), range(0 , n+1)))
-            
+    return list(filter(lambda x: '3' in str(x), range(0, n+1)))
+
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 5 characters long.
+    Returns a list of words in the input text less than 5 characters long
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -76,13 +76,12 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    return list(map(lambda x: x**2, range(1 , n+1)))
-
+    return list(map(lambda x: x**2, range(1, n+1)))
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of lengths of the strings.
 
     >>> lengths([])
     []
@@ -92,4 +91,3 @@ def lengths(strings):
     [4, 2, 1, 4]
     '''
     return list(map(lambda x: len(x), strings))
-
